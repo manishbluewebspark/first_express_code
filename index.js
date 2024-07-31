2b5c3cf6ca475498a0bded56a5e82a8da6058eb6
manish <bluewebspark@gmail.com>
2024-07-31 14:26:54 +0530

    y firstapp

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/myworld', (req, res) => {
+
+res.send('Hello Manish World')
+
+} ) 
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
